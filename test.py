db9e82704fb8f51d71336429e07822ac1e48c7c7
heyrin1998 <qq5200@naver.com>
2023-04-24 22:30:19 +0900

    4월 24일

--- test.py
+++ test.py
@@ -1,4 +1,23 @@
-i, j = 2, 4
-bucket = [1, 2, 3, 4, 5, 6]
-bucket = bucket[:i-1]+bucket[i-1:j][::-1]+bucket[j:]
-print(bucket)
+hp = 999
+answer = 0
+
+if hp // 5:  # hp가 5로 나누어진다면 장군개미 공격 가능
+    answer += hp//5
+    hp = hp % 5
+    if hp // 3:  # hp가 3으로 나누어진다면 장군개미 공격 가능
+        answer += hp//3
+        hp = hp % 3
+        if hp < 3:
+            answer += hp
+    else:
+        answer += hp
+elif hp // 3:
+    answer += hp//3
+    hp = hp % 3
+    if hp < 3:
+        answer += hp
+else:
+    answer += hp
+
+
+print(answer, hp)
